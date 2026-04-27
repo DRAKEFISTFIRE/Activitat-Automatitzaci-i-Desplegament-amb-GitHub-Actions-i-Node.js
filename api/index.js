1bb864f7f19674f66962fcdfc0272a49f39a0948
Aniol Rodríguez <aniolrodriguez2005@gmail.com>
2026-04-27 17:23:54 +0200

    Dades vercel

--- api/index.js
+++ api/index.js
@@ -5,10 +5,12 @@ const app = express();
 
 app.use(express.json());
 
+// Ruta principal API
 app.get('/', (req, res) => {
   res.send('API de viatges funcionant 🚀');
 });
 
+// Rutes
 app.get('/trips', (req, res) => {
   res.json([
     { id: 1, destination: 'Barcelona', days: 3 },
@@ -31,5 +33,4 @@ app.use((req, res) => {
   res.status(404).json({ error: 'Ruta no trobada' });
 });
 
-// 🔥 AIXÒ ÉS LA CLAU
 module.exports = serverless(app);
